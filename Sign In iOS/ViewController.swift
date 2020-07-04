021dfa28560d8162bc5794d3cf6e145850833a4f
pa <pablo.triangle@gmail.com>
2020-07-03 19:53:21 -0500

    working on login

--- Sign In iOS/ViewController.swift
+++ Sign In iOS/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var signIn: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+   // Do any additional setup after loading the view.
+        signIn.layer.cornerRadius = 5;
+        signIn.clipsToBounds = true;
     }
 
 
